3c2d76d329b3c1b44ecbbc22f6ff39726e6a1570
Dii0705 <166929580+Dii0705@users.noreply.github.com>
2024-05-10 20:05:05 -0400

    Time: 41 ms (15.55%) | Memory: 16.6 MB (23.76%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -7,16 +7,15 @@ class MyStack:
         self.stack.append(x)
 
     def pop(self) -> int:
-        for i in range(len(self.stack)-1):
-            self.push(self.stack.popleft())
-        return self.stack.popleft()
+        for i in range(len(self.stack)-1): # pop each value except the last one
+            self.push(self.stack.popleft()) # pop each element and then add to the right
+        return self.stack.popleft() # pop the last element in the queue
 
     def top(self) -> int:
         return self.stack[-1]
 
     def empty(self) -> bool:
         return len(self.stack) == 0
-        
 
 
 # Your MyStack object will be instantiated and called as such:
