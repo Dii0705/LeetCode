925796a8c5e5eacebb7a6d5244dad58d59f86a9e
Diii <166929580+Dii0705@users.noreply.github.com>
2024-05-15 18:40:15 -0400

    Time: 89 ms (56.96%) | Memory: 20.8 MB (89.49%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -8,11 +8,11 @@ class Solution:
         for num, freq in count.items(): # for key, value in count
             heapq.heappush(heap, (freq, num))
 
-            if len(heap) > k:
-                heapq.heappop(heap) # pop the root, which will be the k-smallest
+            if len(heap) > k: # only maintain the kth most frequent
+                heapq.heappop(heap) # remove the smallest
             
         res = []
         while heap:
-            res.append(heapq.heappop(heap)[1])
+            res.append(heapq.heappop(heap)[1]) 
         
         return res
