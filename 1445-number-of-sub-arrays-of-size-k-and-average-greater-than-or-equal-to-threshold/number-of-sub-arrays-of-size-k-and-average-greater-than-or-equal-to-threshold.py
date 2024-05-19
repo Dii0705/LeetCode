82fc6fc6aab41dfdd5cebc7bca25f9a493e2a28b
Diii <166929580+Dii0705@users.noreply.github.com>
2024-05-19 12:05:54 -0400

    Time: 415 ms (99.45%) | Memory: 29.8 MB (46.22%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -4,16 +4,16 @@ class Solution:
         # the sub-arrays average must be greater than or equal  to threshold
         
         # sliding window
-        l = 0
-        curSum = sum(arr[l:l+k])
-        res = 1 if curSum >= threshold * k else 0
+        curSum = sum(arr[:k-1])
+        res = 0
         
-
-        for r in range(k, len(arr)):
-            curSum += arr[r] - arr[l]
-            if r-l+1 > k :
-                l += 1
-            
+        for l in range(len(arr)-k+1):
+            curSum += arr[l+k-1] # add value of "r"
             if curSum >= threshold * k:
                 res += 1
+            curSum -= arr[l]
+
         return res
+        
+
+        
