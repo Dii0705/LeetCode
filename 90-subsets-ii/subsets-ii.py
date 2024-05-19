09b24795a0638975b1e2d23fde6130c81c478548
Diii <166929580+Dii0705@users.noreply.github.com>
2024-05-18 22:28:22 -0400

    Time: 38 ms (65.03%) | Memory: 16.6 MB (90.70%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,24 +1,25 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # array may contain duplicates
         nums.sort()
 
         curSet, subSet = [], []
-
-        def helper(i, nums, curSet, subSet):
-            if i >= len(nums):
+        def backtrack(i, curSet, subSet):
+            if i == len(nums):
                 subSet.append(curSet.copy())
                 return
             
-            # include the nums[i]
             curSet.append(nums[i])
-            helper(i+1, nums, curSet, subSet)
+            backtrack(i+1, curSet, subSet)
             curSet.pop()
+            
 
-            # not include the nums[i]
-            # i.e. we encounter duplicates in the array
             while i+1 < len(nums) and nums[i] == nums[i+1]:
                 i+=1
-            helper(i+1, nums, curSet, subSet)
-        
-        helper(0, nums, curSet, subSet)
+            backtrack(i+1, curSet, subSet)
+
+        if nums:
+            backtrack(0, curSet, subSet)
+
         return subSet
+
