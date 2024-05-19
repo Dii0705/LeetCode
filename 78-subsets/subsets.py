7ef6c909d1f11ae27268270b7d1ad255d987610b
Diii <166929580+Dii0705@users.noreply.github.com>
2024-05-18 22:14:40 -0400

    Time: 33 ms (80.73%) | Memory: 16.7 MB (77.68%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,22 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # nums have unique elements
-        # return all possible subsets (must be subsequent) that does not contain any duplicates
-        # solution can be in any order
-        curSet, subSet = [],[]
-
-        def helper(i, nums, curSet, subSet):
+        # Backtracking technique with Subsets
+        curSet, subSet = [], []
+        def backtrack(i, curSet):
             if i >= len(nums):
                 subSet.append(curSet.copy())
                 return
-            # decision to include nums[i]
-            curSet.append(nums[i])
-            helper(i+1, nums, curSet, subSet)
-            curSet.pop()  # make sure there is no dup
-
-            # decision not to include nums[i]
-            helper(i+1, nums, curSet, subSet)
-        
-        helper(0, nums, curSet, subSet)
-        return subSet
             
+            # include nums[i]
+            curSet.append(nums[i])
+            backtrack(i+1, curSet)
+            curSet.pop()
+
+            # not include nums[i]
+            backtrack(i+1, curSet)
+        backtrack(0, curSet)
+        return subSet
